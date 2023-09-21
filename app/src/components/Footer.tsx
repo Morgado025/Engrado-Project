d428787583e728ef3dcee7704de24b121ad2f4dd
WillianIMS <willianisami1229@gmail.com>
2023-09-21 06:00:50 -0300

    feat: favorite pages with routes

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonRow, IonCol, IonIcon } from '@ionic/react';
+import { useHistory, BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import favorites from '/public/favorites.svg';
 import devices from '/public/devices.svg';
 import automation from '/public/automation.svg';
 
 const Footer: React.FC = () => {
+  const history = useHistory();
+
+  const devicesPage = () => {
+    history.push('/home');
+  };
+
+  const automationPage = () => {
+    history.push('/automation');
+  };
+
+  const favoritesPage = () => {
+    history.push('/favorites');
+  };
+
   return (
     <footer id='footer'>
         <IonRow>
-            <IonCol><IonIcon icon={favorites} />Favoritos</IonCol>
-            <IonCol><IonIcon id='actual' icon={devices} /><span className='actual-page'>Dispositivos</span></IonCol>
-            <IonCol><IonIcon icon={automation} />Automação</IonCol>
+            <IonCol><IonIcon onClick={favoritesPage} icon={favorites} />Favoritos</IonCol>
+            <IonCol><IonIcon onClick={devicesPage} id='actual' icon={devices} /><span className='actual-page'>Dispositivos</span></IonCol>
+            <IonCol><IonIcon onClick={automationPage} icon={automation} />Automação</IonCol>
         </IonRow>
     </footer>
   );
